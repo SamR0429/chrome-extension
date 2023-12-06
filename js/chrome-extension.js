231404003104e27923b118d861ab541ddc744f3f
SamR0429 <Samanatha.ramos01@gmail.com>
2023-12-06 14:15:56 -0600

    cleaned up code layout and working on ability to save url for current tab youre on

--- js/chrome-extension.js
+++ js/chrome-extension.js
@@ -1,9 +1,13 @@
-let myLeads = [];
+let myLinks = [];
 
 const inputEl = document.querySelector(".input-el");
 const inputBtn = document.querySelector(".input-btn");
 const ulEl = document.querySelector(".ul-el");
+const tabBtn = document.querySelector(".tab-btn");
+const deleteBtn = document.querySelector(".delete-btn");
+const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"));
 
+console.log(linksFromLocalStorage);
 // localStorage.setItem("myLeads", "www.samanthalramos.com");
 // let link = localStorage.getItem("myLeads");
 // console.log(link);
@@ -14,41 +18,23 @@ const ulEl = document.querySelector(".ul-el");
 // myLeads = JSON.stringify(myLeads);
 // console.log(typeof myLeads);
 
-// localStorage.clear();
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
-
-if(leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage;
-    renderLeads();
+if (linksFromLocalStorage) {
+    myLinks = linksFromLocalStorage;
+    renderLinks(myLinks);
 }
 
-
-
-inputBtn.addEventListener("click", function () {
-
-    myLeads.push(inputEl.value);
-    inputEl.value = '';
-
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-    renderLeads();
-    console.log(localStorage.getItem("myLeads"));
-});
-
-function renderLeads() {
+function renderLinks(links) {
 
     let listItems = '';
 
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < links.length; i++) {
 
         //this is the original version and we refactored to make code work faster ?? and to clean up the code. this is done to wait until the for loop is done iterating to then render so to not mess with the dom every iteration
         // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${links[i]}'>
+                    ${links[i]}
                 </a>
             </li>
         `
@@ -56,3 +42,38 @@ function renderLeads() {
 
     ulEl.innerHTML = listItems;
 }
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+];
+tabBtn.addEventListener("click", function(){
+    // console.log(tabs[0].url);
+
+    myLinks.push(tabs[0].url);
+    tabBtn.value = '';
+
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+
+    renderLinks(myLinks);
+    console.log(localStorage.getItem("myLinks"));
+
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+
+    localStorage.clear();
+    myLinks = [];
+    renderLinks(myLinks);
+
+});
+
+inputBtn.addEventListener("click", function () {
+
+    myLinks.push(inputEl.value);
+    inputEl.value = '';
+
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+
+    renderLinks(myLinks);
+    console.log(localStorage.getItem("myLinks"));
+});
